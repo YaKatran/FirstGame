e72cff010118ac808eff115a01e4ec1efaa2bba9
YaKatran <dp200592kvd@gmail.com>
2021-07-10 17:26:12 +0300

    111

---
+++ Assets/PlayerMowement.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMowement : MonoBehaviour
+{
+    public Rigidbody rb;
+    [SerializeField]
+    private float speed = 500f;
+    [SerializeField]
+    private float strafeSpeed = 500f;
+    [SerializeField]
+    private float jumpForce = 15f;
+
+    protected bool doJump = false;
+    protected bool strafeLeft = false;
+    protected bool strafeRight = false;
+    protected bool moveForvard = false;
+    protected bool moveBackvard = false;
+
+    void Update()
+    {
+        if (Input.GetKey("s"))
+        {
+            moveBackvard = true;
+        }
+        else
+        {
+            moveBackvard = false;
+        }
+        if (Input.GetKey("w"))
+        {
+            moveForvard = true;
+        }
+        else
+        {
+            moveForvard = false;
+        }
+        if (Input.GetKey("a"))
+        {
+            strafeLeft = true;
+        }
+        else
+        {
+            strafeLeft = false;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            strafeRight = true;
+        }
+        else
+        {
+            strafeRight = false;
+        }
+
+        if (Input.GetKey("space"))
+        {
+            doJump = true;
+        }
+        else
+        {
+            doJump = false;
+        }
+
+        if (transform.position.y < 5f)
+        {
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (moveForvard)
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (moveBackvard)
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+
+        if (strafeLeft)
+        {
+            rb.AddForce(-strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (strafeRight)
+        {
+            rb.AddForce(strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (doJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+    }
+}
